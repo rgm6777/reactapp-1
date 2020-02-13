725e6995849ed37bbc727f53e79f1a2bb584c8c7
rgm6777 <rgm6777work@gmail.com>
2020-02-13 16:35:17 +0530

    13022020.2

--- reactapp/src/containers/Blog/Blog.js
+++ reactapp/src/containers/Blog/Blog.js
@@ -1,59 +1,46 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import axios from '../../axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import { Route, NavLink, Switch } from 'react-router-dom';
+import asyncComponent from '../../hoc/asyncComponent';
 import './Blog.css';
+import Posts from '../Posts/Posts';
+// import NewPost from './../NewPost/NewPost';
+const aysncNewPost = asyncComponent(() => {
+    return import('./../NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        posts: [],
-        selectedPost: null,
-        error: false
-    }
-    componentDidMount() {
-        axios.get('/posts')
-        .then(response => {
-            const posts = response.data.slice(0,4);
-            const updatedPosts = posts.map(post => {
-                return {
-                    ...post,
-                    author: 'Sam'
-                }
-            })
-            this.setState({posts: updatedPosts})
-        })
-        .catch(error => {
-            this.setState({error: true})
-        })
-    }
-    postSelectedHandler = (id) => {
-        this.setState({selectedPost: id});
+        auth: true
     }
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
-        if(!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}/>
-            });
-        }
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPost}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeStyle={{
+                                    color: '#fa923f',
+                                }}>Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Switch>
+                    {this.state.auth ? <Route path="/new-post" component={aysncNewPost} /> : null }
+                    <Route path="/posts" component={Posts} />
+                    {/* <Redirect from="/" to="/posts" /> */}
+                    <Route component={() => <h1 style={{color: 'red', fontSize: '40', textAlign: 'center'}}>Error 404: Page not found!</h1>} />
+                </Switch>
             </div>
         );
     }
--- reactapp/src/containers/NewPost/NewPost.js
+++ reactapp/src/containers/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -7,7 +8,12 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted: false
+    }
+
+    componentDidMount() {
+        console.log(this.props);
     }
 
     postDataHandler = () => {
@@ -16,15 +22,19 @@ class NewPost extends Component {
             body: this.state.body,
             author: this.state.author
         };
-        axios.post('https://jsonplaceholder.typicode.com/posts', post)
+        axios.post('/posts', post)
             .then(response => {
                 console.log(response);
+                // this.setState({submitted: true})
+                this.props.history.push('/posts')
             })
     }
 
     render () {
+        const redirect = <Redirect to="/posts" />
         return (
             <div className="NewPost">
+                {this.state.submitted ? redirect : null}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
